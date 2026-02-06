a1b3df357deab77bec48b16d789b9059d9a86fde
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-06 07:23:00 +0530

    Add DataTypes.cpp to demonstrate variable types

---
+++ DataTypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i=98;
+  float f=98.7;
+  String s="Purna";
+  char ch='K';
+cout<<"Integer:"<<i<<endl<<"Float:"<<f<<endl<<"String:"<<s<<endl<<"Character:"<<ch<<endl;
+}
