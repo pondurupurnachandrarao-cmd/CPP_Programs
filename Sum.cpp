d85d0f9e0cf842d8af47855cc47ac2a053aa7b46
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-07 08:29:53 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"Enter a-value:"<<endl;
+  cin>>a;
+  cout<<"Enter b-value:"<<endl;
+  cin>>b;
+  c=a+b;
+  cout<<"Sum of a+b is:"<<" "<<C;
+}
