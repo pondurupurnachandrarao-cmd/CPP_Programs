9bcd9b4616d19dd2b648999b4a245257309dbe8d
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-08 21:09:14 +0530

    Create Arthimetic.cpp

---
+++ Arthimetic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter first number:";
+  cin>>a;
+  cout<<"Enter ssecond number:";
+  cin>>b;
+  cout<<"Sum:"<<a+b<<endl<<"Difference:"<<a-b<<endl<<"Product:"<<a*b<<endl<<"Division:"<<a/b<<endl<<"Modulus:"<<a%b<<endl;
+
+return 0;
+}
+  
