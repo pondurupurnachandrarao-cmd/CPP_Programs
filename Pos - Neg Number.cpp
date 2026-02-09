98450a4a933a24749079d87789d0112d178b2153
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-09 18:27:02 +0530

    Create Pos - Neg Number.cpp

---
+++ Pos - Neg Number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"entrer a value:";
+  cin>>a;
+  if(a>0){
+      cout<<"Positive number"<<endl;
+  }
+  else if(a<0){
+     cout<<"Negative number"<<endl;
+    }
+    else
+    {
+      cout<<"Zero"<<endl;
+    }
+}
