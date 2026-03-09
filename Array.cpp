083c3f26b37b4741223c79066a3fdd93e7a853e1
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-03-09 22:16:12 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+
+    cout << "Enter 5 numbers:" << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements are:" << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
